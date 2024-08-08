00be0511096a2b0ef450f04f20d327e9e8cb089e
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-08-08 18:43:38 -0300

    Clase 08

---
+++ modulo-javascipr-clase-08/app.js
@@ -0,0 +1,21 @@
+// Metodos de Array
+
+// Dada una palabra por parametro devolver el orden inverso de las letras
+const invertir = (palabra) => {};
+
+// caso de uso: console.log("invertir",invertir("eduardo")) "odraude"
+
+// Dado una palabra y una clave encriptar los caracteres que coincidan con la clave
+const encriptar = (palabra, clave) => {};
+
+// caso de uso: console.log("encriptar",encriptar("eduardo","abuelo")) "3d20rd5"
+
+// Dado una lista de correos y un nuevo correo verificar si dicho existe y sino agregarlo a la lista
+const upsert = (lista, correo) => {};
+
+// caso de uso: console.log("upsert",upsert(["edu@gmail.com","nico@gmail.com"],"edu@gmail.com")) "edu@gmail.com"
+// caso de uso: console.log("upsert",upsert(["edu@gmail.com","nico@gmail.com"],"maria@gmail.com")) ["edu@gmail.com","nico@gmail.com","maria@gmail.com"]
+
+// Predefinir un catalogo de productos y dado un carrito por parametro calcular el valor total
+const productos = [];
+const calcular = (carrito) => {};
