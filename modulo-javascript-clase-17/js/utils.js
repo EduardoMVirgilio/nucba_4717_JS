2ac1f1a2b96cb073d7b11ed1e4f452bf60759c94
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-09-10 21:44:22 -0300

    Clase 17

--- modulo-javascript-clase-17/js/utils.js
+++ modulo-javascript-clase-17/js/utils.js
@@ -1,6 +1,6 @@
 const select = (selector = "") => document.querySelector(selector);
 const selectAll = (selector = "") => document.querySelectorAll(selector);
-const toggle = (element="", className="") => select(element).classList.toggle(className);
+const toggle = (element="", className="") => element.classList.toggle(className);
 const create = (tag="",content="",attributes={}) => {
     const element = document.createElement(tag)
     element.innerHTML=content
