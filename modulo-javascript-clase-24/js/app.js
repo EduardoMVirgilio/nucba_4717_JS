583e8afbc54e1c020dfa01b2cd20b4a072d1caa3
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-10-03 21:34:20 -0300

    End PokeApp | Clase 24

--- modulo-javascript-clase-24/js/app.js
+++ modulo-javascript-clase-24/js/app.js
@@ -1,2 +1,51 @@
+import { getList } from "./poke.js";
 import { create, save, read } from "./utils.js";
+const list = document.querySelector("#list")
+const page = document.querySelector("#page")
+const btnPrev = document.querySelector("#btnPrev")
+const btnNext = document.querySelector("#btnNext")
+
+
+const renderPokemons = (pokemons = []) => {
+    list.innerHTML = null
+    pokemons.forEach(pokemon => {
+        list.append(create("li", `<picture><img src="${pokemon.image}"/></picture><h2>${pokemon.name}</h2><dl>${pokemon.types.map((type) => `<dd style="background:${type.color}">${type.name}</dd>`).join("")}<dl>`))
+    });
+}
+
+const render = (data = null) => {
+    if (!data) {
+        return getList().then(result => {
+            if (result.next != undefined) {
+                btnNext.addEventListener('click', () => {
+                    getList(result.next).then(result => render(result))
+                })
+            }
+            if (result.prev != undefined) {
+                btnPrev.addEventListener('click', () => {
+                    getList(result.prev).then(result => render(result))
+                })
+            }
+            page.innerHTML = result.page + 1
+            return renderPokemons(result.pokemons)
+
+        })
+    }
+
+    if (data.next != undefined) {
+        btnNext.addEventListener('click', () => {
+            getList(data.next).then(result => render(result))
+        })
+    }
+    if (data.prev != undefined) {
+        btnPrev.addEventListener('click', () => {
+            getList(data.prev).then(result => render(result))
+        })
+    }
+    page.innerHTML = data.page + 1
+    return renderPokemons(data.pokemons)
+}
+
+
+render()
 
--- modulo-javascript-clase-24/js/poke.js
+++ modulo-javascript-clase-24/js/poke.js
@@ -1,5 +1,59 @@
-export const getList = async (type = null) => {
+const baseURL = "https://pokeapi.co/api/v2/"
+export const getList = async (page = 0) => {
+    let result = {
+        page: 0,
+        next: 1,
+        prev: 0
+    }
+    let quantity = 8
+    try {
+        const query = await fetch(`${baseURL}/pokemon?limit=${quantity}&offset=${quantity * page}`)
+        const res = await query.json()
+        if (res.next) {
+            result.next = page + 1
+        }
+        if (res.previous) {
+            result.prev = page - 1
+        }
+        result.page = page
+        result.pokemons = []
+        for await (const resultado of res.results) {
+            const queryPokemon = await fetch(`${resultado.url}`)
+            const pokemon = await queryPokemon.json()
+            const colors = {
+                normal: "#aa9",
+                fire: "#f42",
+                grass: "#7c5",
+                poison: "#a59",
+                water: "#39f",
+                electric: "#fc3",
+                ice: "#6cf",
+                fighting: "#b54",
+                ground: "#db5",
+                flying: "#89f",
+                psychic: "#f59",
+                bug: "#ab2",
+                rock: "#ba6",
+                ghost: "#66b",
+                dragon: "#76e",
+                dark: "#070301",
+                steel: "#aab",
+                fairy: "#e9e"
+            }
 
+            result.pokemons.push({
+                id: pokemon.id,
+                name: pokemon.name,
+                height: pokemon.height,
+                weight: pokemon.weight,
+                experience: pokemon.base_experience,
+                image: pokemon.sprites.other["official-artwork"]["front_default"],
+                types: pokemon.types.map(({ type }) => ({ name: type.name, color: colors[type.name] }))
+            })
+        }
+        return result
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-export const getOne = async (id) => { }
