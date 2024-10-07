578b51d12d3771167ba34a121a35bffa16971636
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-10-07 17:34:05 -0300

    Init Weather App | Clase 25

---
+++ modulo-javascript-clase-25/app.js
@@ -0,0 +1,5 @@
+/*
+    https://restcountries.com/v3.1/subregion/South America?fields=name,capital,flags
+    https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires&appid=c76b349514ffd530307eae47f8f747be
+    https://bas.dev/work/meteocons
+*/
