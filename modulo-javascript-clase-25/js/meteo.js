6dc3a8d31b1485ead82ec41a66746c853e7b0471
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-10-08 19:00:39 -0300

    Init Weather App

--- modulo-javascript-clase-25/js/meteo.js
+++ modulo-javascript-clase-25/js/meteo.js
@@ -3,7 +3,7 @@
 import { request } from "./utils.js"
 
 const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather'
-const appid = 'c76b349514ffd530307eae47f8f747be'
+const appid = ''
 const units = 'metric'
 const lang = 'es'
 const getWeather = async (lat, lon) => { }
