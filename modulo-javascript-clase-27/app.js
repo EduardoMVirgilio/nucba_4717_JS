4743cc3e682c840504bc003b35bcf09f0803ff80
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-10-15 20:24:48 -0300

    Fin Clase 27

--- modulo-javascript-clase-27/app.js
+++ modulo-javascript-clase-27/app.js
@@ -5,9 +5,44 @@ let loading = true;
 const content = select('#list');
 const loader = select('.loader');
 
-const listaPersonajes = async () => { }
+const listaPersonajes = async () => {
+    try {
+        let { info, results } = url != null ? await request(url) : { info: null, results: [] }
+        url = info?.next
 
-const observer = null;
+        if (results.length == 0) {
+            loader.style.display = "none"
+        }
+
+        return results.forEach((resultado) => {
+            content.append(create('li', `<img src="${resultado.image}"/> <h2>${resultado.name}<h2><p>${resultado.species}</p>`))
+        })
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
+/*
+window.addEventListener("scroll", async (e) => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    const bottom = scrollTop + clientHeight >= scrollHeight - 1;
+    if (bottom && !loading) {
+        loading = true;
+        await listaPersonajes()
+        loading = false
+    }
+})
+*/
+
+const observer = new IntersectionObserver(async (entries) => {
+    if (entries[0].isIntersecting && !loading) {
+        loading = true;
+        await listaPersonajes()
+        loading = false
+    }
+}, { threshold: 0.25 });
+
+observer.observe(loader)
 
 listaPersonajes()
 
